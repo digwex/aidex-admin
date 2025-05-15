82fb50140224227f5a3431087c176e0f14187c1e
Eugene <sifmeop@gmail.com>
2025-05-15 21:24:10 +0300

    getInitialSearchDate: change to seconds

--- src/utils/getInitialSearchDate.ts
+++ src/utils/getInitialSearchDate.ts
@@ -1,6 +1,6 @@
 export const getInitialSearchDate = (): [number, number] => {
-  const seconds10YearsAgo = Math.floor(Date.now() - 10 * 365 * 24 * 60 * 60 * 1000)
-  const endOfDaySeconds = Math.floor(new Date().setHours(23, 59, 59, 999))
+  const seconds10YearsAgo = Math.floor((Date.now() - 10 * 365 * 24 * 60 * 60 * 1000) / 1000)
+  const endOfDaySeconds = Math.floor(new Date().setHours(23, 59, 59, 999) / 1000)
 
   return [seconds10YearsAgo, endOfDaySeconds]
 }
